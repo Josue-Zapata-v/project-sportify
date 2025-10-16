a25abbc1145a7029d7571d102c2c7133c3be908e
Josue-Zapata-v <zjosue775@gmail.com>
2025-10-15 21:51:16 -0500

    serializer-clase EquipoSerializer agregado

--- src/sportify_app/serializers.py
+++ src/sportify_app/serializers.py
@@ -6,3 +6,9 @@ class JugadorSerializer(serializers.ModelSerializer):
         model = Jugador
         fields = ['id', 'nombre', 'posicion', 'numero_camiseta', 'equipo']
 
+class EquipoSerializer(serializers.ModelSerializer):
+    jugadores = JugadorSerializer(many=True, read_only=True) 
+
+    class Meta:
+        model = Equipo
+        fields = ['id', 'nombre', 'ciudad', 'categoria', 'jugadores']
