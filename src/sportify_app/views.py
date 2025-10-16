23a08f1a4cb2143c17b07214c3bed9365f9438db
Josue-Zapata-v <zjosue775@gmail.com>
2025-10-15 21:58:01 -0500

    views.py-class EquipoViewSet agregado

--- src/sportify_app/views.py
+++ src/sportify_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Equipo, Jugador
+from .serializers import EquipoSerializer, JugadorSerializer
 
 # Create your views here.
+class EquipoViewSet(viewsets.ModelViewSet):
+    queryset = Equipo.objects.all()
+    serializer_class = EquipoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nombre', 'categoria']
