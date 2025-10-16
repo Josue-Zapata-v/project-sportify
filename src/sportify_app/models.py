f2264922384edf4fad360be8fb67ac7ddcf62de2
Josue-Zapata-v <zjosue775@gmail.com>
2025-10-15 21:44:17 -0500

    entidad 2- entidades completas

--- src/sportify_app/models.py
+++ src/sportify_app/models.py
@@ -8,3 +8,16 @@ class Equipo(models.Model):
 
     def __str__(self):
         return f"{self.nombre} ({self.ciudad})"
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=100)
+    posicion = models.CharField(max_length=50)
+    numero_camiseta = models.PositiveIntegerField()
+    equipo = models.ForeignKey(
+        Equipo, 
+        related_name="jugadores", 
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.nombre} - {self.posicion} #{self.numero_camiseta}"
