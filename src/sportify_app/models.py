536c2ca7e68389714a9a6b67a1432eeaab0fe4e2
Josue-Zapata-v <zjosue775@gmail.com>
2025-10-15 21:41:40 -0500

    entidad Equipo creada

--- src/sportify_app/models.py
+++ src/sportify_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.ciudad})"
