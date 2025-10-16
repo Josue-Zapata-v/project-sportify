ac13c5e8ae4b91b03616175c980c60b373f96b3c
Josue-Zapata-v <zjosue775@gmail.com>
2025-10-15 23:29:29 -0500

    terminado

--- src/sportify_app/views.py
+++ src/sportify_app/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, filters
 from .models import Equipo, Jugador
 from .serializers import EquipoSerializer, JugadorSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 class EquipoViewSet(viewsets.ModelViewSet):
@@ -8,6 +10,11 @@ class EquipoViewSet(viewsets.ModelViewSet):
     serializer_class = EquipoSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['nombre', 'categoria']
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "Equipo eliminado correctamente"}, status=status.HTTP_200_OK)
     
 class JugadorViewSet(viewsets.ModelViewSet):
     queryset = Jugador.objects.all()
